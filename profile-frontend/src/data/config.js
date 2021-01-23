946db0fc11e41bb7fe630b15e982a0a785a43c69
Cyb3rM3rlin <starlordkrishna05@gmail.com>
2021-01-23 07:04:02 +0000

    final

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
   // TODO: change your profile information here
-  name: "Ashok Kumar",
+  name: "Krishnaprasad",
   greeting: "Hey 👋",
-  greetingDescription: "I'm Ashok Kumar and I'm a Software Engineer!",
-  githubUrl: "https://github.com/ashokkumar95",
-  linkedinUrl: "https://linkedin.com/ashokkumar1",
-  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+  greetingDescription: "I'm Krishnaprasad and I'm a Software Engineer!",
+  githubUrl: "https://github.com/cyb3rm3rlin",
+  linkedinUrl: "https://www.linkedin.com/in/krishna-prasad-nayani-a9baa7163/",
+  cvLink: "",
 };
